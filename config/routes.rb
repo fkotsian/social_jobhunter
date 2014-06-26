63aada34fcbbb935a835f4eb956209f39996bc12
Frank Kotsianas <frankkotsianas@gmail.com>
2014-06-25 18:59:44 -0700

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+
+  resources :jobs, only: [:index], as: 'all_jobs'
+  scope :user do
+    resources :jobs, except: [:index]
+  end
+  
+  
   root to: 'static_pages#home'
+  # devise_scope :user do
+  #   root to: 'devise/sessions#new'
+  # end
 end
