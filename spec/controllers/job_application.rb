f8746bc7c4c0e4b6ef6b380774162c758b116f32
Frank Kotsianas <frankkotsianas@gmail.com>
2014-07-10 00:04:18 -0700

    Add basic JobApplication controller specs

---
+++ spec/controllers/job_application.rb
@@ -0,0 +1,14 @@
+describe JobApplicationsController do
+  describe "CREATE new application" do
+    it "creates a valid job_application" do
+      app = JobApplication.create
+      get :index
+      expect(assigns(:job_applications)).to eq( [app] )
+    end
+
+    it "creates a new Job if one does not exist"
+    
+    it "does not create a new Job if one does already exist"
+
+  end
+end
