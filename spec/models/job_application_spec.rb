b682e91b864adf8ae4a947a6b5c476e86a38e963
Frank Kotsianas <frankkotsianas@gmail.com>
2014-07-09 23:56:01 -0700

    Round out basic JobApplication model specs

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe JobApplication do
-  
-  let(:applicant) { FactoryGirl.create(:user) }
-  let(:job)       { FactoryGirl.create(:job) }
 
-  it "has a valid factory" do
-    FactoryGirl.create(:job_application).should be_valid
+  let(:app)   { FactoryGirl.create(:job_application) }
+  
+  context "validations" do
+    it "has a valid factory" do
+      FactoryGirl.create(:job_application).should be_valid
+    end
+    it "belongs to a job" do
+      expect(app.job_id).to be_a Integer
+    end
+    it "belongs to an applicant" do
+      expect(app.applicant.id).to be_a Integer
+    end
+    it "has a status" do
+      expect(app.status).to eq("Applied")
+    end
   end
-  it "belongs to a job"
-  it "belongs to an applicant"
-  it "has a status"
   
   context "create a new application" do
     it "creates a job if necessary"
   end
+  
 end
