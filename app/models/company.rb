eecf7dc8bc30289d899b4a048b7204747ccf814a
Frank Kotsianas <frankkotsianas@gmail.com>
2014-07-12 14:15:17 -0700

    Add Url to companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  validates :url, uniqueness: true
   
   has_many :jobs
   # has_many :applications, through: :jobs, source: :job_applications
