8b244f9378bb873d93f2f516f0e4f8098c6ac098
Frank Kotsianas <frankkotsianas@gmail.com>
2014-07-12 20:36:24 -0700

    Make job categories display nice

--- app/models/job_category.rb
+++ app/models/job_category.rb
@@ -1,6 +1,10 @@
-class JobCategory < ActiveRecord::Base
+class JobCategory < ActiveRecord::Base  
   has_many :jobs
   validates :name, uniqueness: true
   
   attr_reader :name
+  
+  def self.names
+    categories = self.all.pluck(:name)
+  end
 end
