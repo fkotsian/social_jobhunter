ad7790bf8e5223168157bf1896dd9a28760e81f8
Frank Kotsianas <frankkotsianas@gmail.com>
2014-07-15 20:36:29 -0700

    Add category_applications routes and find

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,26 @@ Rails.application.routes.draw do
   resources :companies, only: [:index]
   scope :user do
     get '/applications', to: 'job_applications#index', as: 'my_applications'
-    resources :job_applications, only: [:new, :create]
+    resources :job_applications, only: [:create]
     resources :jobs, except: [:index] do
-      resources :job_applications, except: [:index, :new, :create], as: 'applications'
+      resources :job_applications, 
+          except: [:index, :create], 
+          as: 'applications'
     end
+    
+    # get 'applications/:company_id',
+    #       to: 'job_applications#company_applications',
+    #       as: 'company_applications'
+    get 'applications/:category_id',
+          to: 'job_applications#category_applications',
+          as: 'category_applications'
+    get 'applications/:area_id', 
+          to: 'job_applications#area_applications', 
+          as: 'area_applications'      
   end
   resources :jobs, only: [:index], as: 'all_jobs'
-  get 'jobs/:company_id', to: 'jobs#company_jobs', as: 'company_jobs'
   get 'jobs/:category_id', to: 'jobs#category_jobs', as: 'category_jobs'
+  get 'jobs/:company_id', to: 'jobs#company_jobs', as: 'company_jobs'
   get 'jobs/:area_id', to: 'jobs#area_jobs', as: 'area_jobs'  
   
   devise_scope :user do
