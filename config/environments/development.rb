f233328e8c500f974699af5e0b1cc6a489f55c40
Frank Kotsianas <frankkotsianas@gmail.com>
2015-06-14 15:58:11 -0700

    Serve_static_assets has been renamed to serve_static_files.
    Already configured in production and test.
    
    Remove.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
-  # Serve static assets w/o using proxy. 
-  # Replace when set up CDN.
-  config.serve_static_assets = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
